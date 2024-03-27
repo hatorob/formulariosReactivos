f9c32dc0ef675d02de10f29985054dfe567dcb1b
Hector Toro <alejotoro94@hotmail.com>
2024-03-27 11:37:49 -0500

    Se crean las rutas de nuestra app, rutas hijas de auth y reactive, usando loadlazy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'reactive',
+    loadChildren: () => import('./reactive/reactive-routing.module').then( m => m.ReactiveRoutingModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth-routing.module').then( m => m.AuthRoutingModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'reactive',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
