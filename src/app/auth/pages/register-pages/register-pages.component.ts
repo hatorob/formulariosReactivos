0f6fca4e458c16bd08f2094e36bea15a93ab8d33
Hector Toro <alejotoro94@hotmail.com>
2024-03-30 15:44:49 -0500

    Formulario de registro inicial

--- src/app/auth/pages/register-pages/register-pages.component.ts
+++ src/app/auth/pages/register-pages/register-pages.component.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-pages.component.html',
   styles: ``
 })
 export class RegisterPagesComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required ]],
+    email: ['', [ Validators.required ]],
+    userName: ['', [ Validators.required ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    confirmedPassword:  ['', [ Validators.required ]]
+  })
+
+  constructor( private fb: FormBuilder ) {
+
+  }
+
+  isValidField() {
+    // TODO: Obtener validación desde un servicio
+  }
+
+  onSubimit(): void {
+
+    if( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+  }
 
 }
