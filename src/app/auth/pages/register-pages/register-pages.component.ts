26035ba9e4dac5e253415c1b792853beaf7d067b
Hector Toro <alejotoro94@hotmail.com>
2024-03-31 17:01:27 -0500

    Validator service

--- src/app/auth/pages/register-pages/register-pages.component.ts
+++ src/app/auth/pages/register-pages/register-pages.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 //import { cantBeStrider, emailPattern } from '../../../shared/validators/validators.functions';
 //! Podemos usar la importanciones así también
-import * as customValidators from '../../../shared/validators/validators.functions';
+//import * as customValidators from '../../../shared/validators/validators.functions';
+import { ValidatorsService } from '../../../shared/services/validators.service';
 
 @Component({
   templateUrl: './register-pages.component.html',
@@ -11,19 +12,23 @@ import * as customValidators from '../../../shared/validators/validators.functio
 export class RegisterPagesComponent {
 
   public myForm: FormGroup = this.fb.group({
-    name: ['', [ Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern) ]],
-    email: ['', [ Validators.required, Validators.pattern(customValidators.emailPattern) ]],
-    userName: ['', [ Validators.required, customValidators.cantBeStrider ]],
+    name: ['', [ Validators.required, Validators.pattern(this.validatorService.firstNameAndLastnamePattern) ]],
+    email: ['', [ Validators.required, Validators.pattern(this.validatorService.emailPattern) ]],
+    userName: ['', [ Validators.required, this.validatorService.cantBeStrider ]],
     password: ['', [ Validators.required, Validators.minLength(6) ]],
     confirmedPassword:  ['', [ Validators.required ]]
   })
 
-  constructor( private fb: FormBuilder ) {
+  constructor(
+    private fb: FormBuilder,
+    private validatorService: ValidatorsService
+  ) {
 
   }
 
-  isValidField() {
+  isValidField( field: string ) {
     // TODO: Obtener validación desde un servicio
+    return this.validatorService.isValidField(this.myForm, field);
   }
 
   onSubimit(): void {
