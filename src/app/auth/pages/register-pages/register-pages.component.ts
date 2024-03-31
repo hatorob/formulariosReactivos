db5e5996cc88e6260e92dcc0d0c45cd5ae14c62d
Hector Toro <alejotoro94@hotmail.com>
2024-03-31 11:09:33 -0500

    Validaciones de correo, patrones y uso de expresiones regulares

--- src/app/auth/pages/register-pages/register-pages.component.ts
+++ src/app/auth/pages/register-pages/register-pages.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { cantBeStrider } from '../../../shared/validators/validators.functions';
+//import { cantBeStrider, emailPattern } from '../../../shared/validators/validators.functions';
+//! Podemos usar la importanciones así también
+import * as customValidators from '../../../shared/validators/validators.functions';
 
 @Component({
   templateUrl: './register-pages.component.html',
@@ -9,9 +11,9 @@ import { cantBeStrider } from '../../../shared/validators/validators.functions';
 export class RegisterPagesComponent {
 
   public myForm: FormGroup = this.fb.group({
-    name: ['', [ Validators.required ]],
-    email: ['', [ Validators.required ]],
-    userName: ['', [ Validators.required, cantBeStrider ]],
+    name: ['', [ Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern) ]],
+    email: ['', [ Validators.required, Validators.pattern(customValidators.emailPattern) ]],
+    userName: ['', [ Validators.required, customValidators.cantBeStrider ]],
     password: ['', [ Validators.required, Validators.minLength(6) ]],
     confirmedPassword:  ['', [ Validators.required ]]
   })
--- src/app/shared/validators/validators.functions.ts
+++ src/app/shared/validators/validators.functions.ts
@@ -1,7 +1,13 @@
-import { FormControl } from "@angular/forms";
+import { FormControl, ValidationErrors } from "@angular/forms";
+
+//! Validaciones de email
+export const emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+//! Validiaciones del nombre
+export const firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
 
 
-export const cantBeStrider = (control: FormControl) => {
+
+export const cantBeStrider = (control: FormControl): ValidationErrors | null => {
 
   const value: string = control.value.trim().toLowerCase();
 
